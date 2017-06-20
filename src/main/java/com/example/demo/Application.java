13da7c960bcb115a43e759037a574faeb4fb6027
student <student@GBTC441J04LX.montgomerycollege.edu>
2017-06-20 17:43:57 -0400

    fixthis

---
+++ src/main/java/com/example/demo/Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+    	//Greeting x=new Greeting(1, "henok");
+        SpringApplication.run(Application.class, args);
+    }
+   
+}
